8c9bd4ee13a05192e02b04a7a7639895b3252c8c
Brian-Almendras-SC <brian.almendras@ucb.edu.bo>
2021-10-22 15:52:22 -0400

    Update project

---
+++ PracticeDesignPatternStructural/PracticeDesignPatternStructural/Class/Sound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeDesignPatternStructural.Class
+{
+    public abstract class Sound
+    {
+        public string TypeSound { set; get; }
+        public int TimeInSeconds { set; get; }
+        public string Title { set; get; }
+        public void AssignamentToDetails(int Time,string Title) 
+        { 
+            TimeInSeconds = Time;
+            this.Title = Title;
+        }
+        public abstract string ReturnTypeOfSound();
+        public abstract Mp3 ConvertToMp3(Sound Track);
+        public abstract Wma ConvertToWma(Sound Track);
+        public string AboutTrack()
+        {
+            return $"El titulo es: {Title}, Duraccion es: {TimeInSeconds}, El formato del audio es: {TypeSound}";
+        }
+
+    }
+}
---
+++ PracticeDesignPatternStructural/PracticeDesignPatternStructural/Class/Video.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeDesignPatternStructural.Class
+{
+    public abstract class Video
+    {
+        public string TypeVideo { set; get; }
+        public int TimeInSeconds { set; get; }
+        public string Title { set; get; }
+        public Sound TypeSound;
+        public void AssignamentToDetails(int Time, string Tittle,Sound Sound)
+        {
+            TypeSound = Sound;
+            TimeInSeconds = Time;
+            this.Title = Title;
+            TypeSound.AssignamentToDetails(Time, Tittle);
+        }
+        public abstract string ReturnTypeOfVideo();
+        public abstract string ReturnTypeOfVideoAndSound();
+        public abstract Mp4 ConvertToMp4(Video video);
+        public abstract Avi ConvertToAvi(Video video);
+        public string AboutVideo()
+        {
+            return $"El titulo es: {Title}, Duraccion es: {TimeInSeconds}, El formato del video es: {TypeVideo}. {TypeSound.AboutTrack()} ";
+        }
+        public Sound ExtractSound()
+        {
+            return TypeSound;
+        }
+    }
+}
